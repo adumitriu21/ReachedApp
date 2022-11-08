3b89a5b8861203a192476e389b49a44327886bbb
barnau21 <adumitriu21@gmail.com>
2022-11-08 23:14:04 +0200

    initial commit, connected to firebase

---
+++ app/src/main/java/com/example/reachedapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.reachedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("Student");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                List StudentList = new ArrayList<String>();
+
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    Student s = dsp.getValue(Student.class);
+                    StudentList.add(s);
+                    System.out.println(s.toString());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+}
