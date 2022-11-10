f0524fef4227bad62edfbfd26ad1738e00c01563
adumi <adumitriu21@gmail.com>
2022-11-10 10:59:58 +0200

    converted Main Activity to Kotlin code, created TeacherMainMenu fragment. Set up Navigation between fragments. Installed Kotlin dependencies and rebuilt project without errors.

---
+++ app/src/main/java/com/example/reachedapp/Fragments/TeacherMainMenu.kt
@@ -0,0 +1,31 @@
+package com.example.reachedapp.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.reachedapp.R
+
+
+class TeacherMainMenu : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_teacher_main_menu, container, false)
+
+        val attendanceBtn = view.findViewById<Button>(R.id.take_attendance_btn)
+
+        attendanceBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_teacherMainMenu_to_teacherAttendanceView)
+        }
+
+        return view
+    }
+
+
+
+}
--- app/src/main/java/com/example/reachedapp/MainActivity.kt
+++ app/src/main/java/com/example/reachedapp/MainActivity.kt
@@ -1,47 +1,44 @@
-package com.example.reachedapp;
+package com.example.reachedapp
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.DataSnapshot
+import com.example.reachedapp.Models.Student
+import com.google.firebase.database.DatabaseError
+import java.util.ArrayList
 
-import com.example.reachedapp.Models.Student;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-import java.util.ArrayList;
-import java.util.List;
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("Student")
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("Student");
 
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                List StudentList = new ArrayList<String>();
-
-                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
-                    Student s = dsp.getValue(Student.class);
-                    StudentList.add(s);
-                    System.out.println(s.toString());
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val studentList: MutableList<Student> = ArrayList<Student>()
+                for (dsp in dataSnapshot.children) {
+                    val s = dsp.getValue(Student::class.java)
+                    if (s != null) {
+                        studentList.add(s)
+                    }
+                    println(s.toString())
                 }
-
             }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("The read failed: " + databaseError.code)
             }
-        });
+        })
+
     }
 }
